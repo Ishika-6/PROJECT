a5caf7db7e84d0075cbd0c1ef51e6aab55244dfe
harshpreet931 <hsanime@gmail.com>
2024-03-06 20:24:54 +0530

    Add user.json file and update sign-up and sign-in functionality

--- LoginSignup/KTS_LoginSignup_index.js
+++ LoginSignup/KTS_LoginSignup_index.js
@@ -1,4 +1,38 @@
 let container = document.getElementById('container')
+let signUpForm = document.getElementById('signUpForm');
+let signInForm = document.getElementById('signInForm');
+let users = JSON.parse(localStorage.getItem('users')) || [];
+
+signUpForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const username = document.getElementById('su_username').value;
+	const email = document.getElementById('su_email').value;
+	const password = document.getElementById('su_password').value;
+	const user = {username, email, password};
+	users.push(user);
+	localStorage.setItem('users', JSON.stringify(users));
+	alert('Sign up successful');
+	signUpForm.reset();
+});
+
+signInForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const username = document.getElementById('si_username').value;
+	const password = document.getElementById('si_password').value;
+	const user = users.find(
+		(user) => user.username === username && user.password === password
+	);
+
+	if (user) {
+		alert('Sign in successful');
+		signInForm.reset();
+		window.location.href = '../index.html';
+	}
+	else {
+		alert('Invalid username or password');
+	}
+})
+
 
 toggle = () => {
 	container.classList.toggle('sign-in')
