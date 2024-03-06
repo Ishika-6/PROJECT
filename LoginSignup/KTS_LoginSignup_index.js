d1f204f630eab5ee8f247d8ded89552610c0d93d
harshpreet931 <hsanime@gmail.com>
2024-03-06 21:15:40 +0530

    Remove unnecessary alert message in sign-in form

--- LoginSignup/KTS_LoginSignup_index.js
+++ LoginSignup/KTS_LoginSignup_index.js
@@ -24,7 +24,6 @@ signInForm.addEventListener('submit', (e) => {
 	);
 
 	if (user) {
-		alert('Sign in successful');
 		signInForm.reset();
 		window.location.href = '../dashboard/main_dashboard.html';
 	}
